42595a34be35ca2252446eb1974c83ebb854cc1c
Jacob Lal <jacobandeth@gmail.com>
2025-12-04 10:33:27 +0530

    created book_card widget

---
+++ booknest_frontend/lib/widgets/book_card.dart
@@ -0,0 +1,100 @@
+import 'package:flutter/material.dart';
+import 'package:booknest_frontend/models/book.dart';
+// ignore: unused_import
+import 'package:booknest_frontend/services/api_service.dart';
+
+class BookCard extends StatelessWidget {
+  final Book book;
+  final VoidCallback? onTap;
+
+  const BookCard({super.key, required this.book, this.onTap});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: 160,
+      height: 260,
+      child: Card(
+        elevation: 3,
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
+        child: InkWell(
+          onTap: onTap,
+          borderRadius: BorderRadius.circular(12),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              // FIXED IMAGE
+              Container(
+                height: 130,
+                width: double.infinity,
+                decoration: BoxDecoration(
+                  borderRadius: const BorderRadius.vertical(
+                    top: Radius.circular(12),
+                  ),
+                  image: book.cover != null
+                      ? DecorationImage(
+                          image: NetworkImage("${book.cover}"),
+                          fit: BoxFit.cover,
+                        )
+                      : const DecorationImage(
+                          image: AssetImage(
+                            "assets/images/book_placeholder.png",
+                          ),
+                          fit: BoxFit.cover,
+                        ),
+                ),
+              ),
+
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Text(
+                  book.title,
+                  maxLines: 2,
+                  overflow: TextOverflow.ellipsis,
+                  style: const TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 14,
+                  ),
+                ),
+              ),
+
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 8),
+                child: Text(
+                  book.author,
+                  maxLines: 1,
+                  overflow: TextOverflow.ellipsis,
+                  style: TextStyle(fontSize: 12, color: Colors.grey.shade700),
+                ),
+              ),
+
+              const Spacer(),
+
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Container(
+                  padding: const EdgeInsets.symmetric(
+                    vertical: 4,
+                    horizontal: 12,
+                  ),
+                  decoration: BoxDecoration(
+                    color: Colors.deepPurple.shade50,
+                    borderRadius: BorderRadius.circular(50),
+                  ),
+                  child: Text(
+                    book.availableFor.toUpperCase(),
+                    style: const TextStyle(
+                      fontSize: 11,
+                      fontWeight: FontWeight.w600,
+                      color: Colors.deepPurple,
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
