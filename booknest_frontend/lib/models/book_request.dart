ddd160ef224c2629f933b6d78f6a0bb1272be4cb
Jacob Lal <jacobandeth@gmail.com>
2026-01-08 10:57:38 +0530

    added user details(requester & owner)

--- booknest_frontend/lib/models/book_request.dart
+++ booknest_frontend/lib/models/book_request.dart
@@ -2,8 +2,15 @@ class BookRequest {
   final int id;
   final String requestType;
   final String status;
+
+  // Book info
   final String bookTitle;
   final String? bookCover;
+
+  // User info (NEW)
+  final String requesterUsername;
+  final String bookOwnerUsername;
+
   final DateTime createdAt;
 
   BookRequest({
@@ -12,6 +19,8 @@ class BookRequest {
     required this.status,
     required this.bookTitle,
     this.bookCover,
+    required this.requesterUsername,
+    required this.bookOwnerUsername,
     required this.createdAt,
   });
 
@@ -20,8 +29,13 @@ class BookRequest {
       id: json['id'],
       requestType: json['request_type'],
       status: json['status'],
+
       bookTitle: json['book']['title'],
       bookCover: json['book']['cover'],
+
+      requesterUsername: json['requester']['username'],
+      bookOwnerUsername: json['book']['owner']['username'],
+
       createdAt: DateTime.parse(json['created_at']),
     );
   }
