4df04d0c7d34a3a6815821ff0cfb85d7c077ceb8
Jacob Lal <jacobandeth@gmail.com>
2026-01-15 09:50:01 +0530

    updated book class

--- booknest_frontend/lib/models/book_request.dart
+++ booknest_frontend/lib/models/book_request.dart
@@ -2,15 +2,10 @@ class BookRequest {
   final int id;
   final String requestType;
   final String status;
-
-  // Book info
   final String bookTitle;
   final String? bookCover;
-
-  // User info (NEW)
   final String requesterUsername;
   final String bookOwnerUsername;
-
   final DateTime createdAt;
 
   BookRequest({
@@ -29,13 +24,10 @@ class BookRequest {
       id: json['id'],
       requestType: json['request_type'],
       status: json['status'],
-
-      bookTitle: json['book']['title'],
-      bookCover: json['book']['cover'],
-
-      requesterUsername: json['requester']['username'],
-      bookOwnerUsername: json['book']['owner']['username'],
-
+      bookTitle: json['book_title'],
+      bookCover: json['book_cover'],
+      requesterUsername: json['requester_username'],
+      bookOwnerUsername: json['book_owner_username'],
       createdAt: DateTime.parse(json['created_at']),
     );
   }
